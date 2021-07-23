630fbf09eb5546aac87c50e152aa0f0415296c67
dewaary <dewa.g.ari28@gmail.com>
2021-07-23 07:56:38 +0700

    String Library Instance method

---
+++ string.js
@@ -0,0 +1,13 @@
+// Stirng Instance Method and Properties
+const fullName = "Dewa Gede Agung Ari Septiyadi";
+
+console.log(fullName.length);
+console.log(fullName.toLowerCase());
+console.log(fullName.toUpperCase());
+console.log(fullName.split(" "))
+
+
+//Menghapus spasi dikiri dan kanan menggunakan library trim()
+
+const input = "         Input        ";
+console.log(input.trim());
